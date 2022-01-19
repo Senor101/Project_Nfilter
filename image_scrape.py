5eadd3ab08dfcd605493449e803820ccab941ed4
Dikshyanta Aryal <adikshyanta456@gmail.com>
2022-01-19 23:06:08 +0545

    automation downloading images

---
+++ image_scrape.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+import os
+import urllib.request
+import time
+
+path = r'C:\Program Files (x86)\chromedriver.exe'
+
+url_prefix = "https://www.google.com.sg/search?q="
+url_postfix = "&source=lnms&tbm=isch&sa=X&ei=0eZEVbj3IJG5uATalICQAQ&ved=0CAcQ_AUoAQ&biw=939&bih=591"
+
+save_folder = 'train'
+
+def main():
+    if not os.path.exists(save_folder):
+        os.mkdir(save_folder)
+    download_images()
+    
+def download_images():
+    #no of images you want
+    n=2;
+
+    #keyword for what you are searching 
+    search_for='nude chicks'
+    
+    search_url = url_prefix+search_for+url_postfix
+    
+    path = r'C:\Program Files (x86)\chromedriver.exe'
+    
+    driver = webdriver.Chrome(path)
+    driver.get(search_url)
+    
+    value = 0
+    for i in range(3):
+        driver.execute_script("scrollBy("+ str(value) +",+1000);")
+        value += 1000
+        time.sleep(1)
+    
+    elem1 = driver.find_element_by_id('islmp')
+    sub = elem1.find_elements_by_tag_name('img')
+    
+    count=0
+    for j,i in enumerate(sub):
+        if j < n:
+            src = i.get_attribute('src')                         
+            try:
+                if src != None:
+                    src  = str(src)
+                    
+                    urllib.request.urlretrieve(src, os.path.join(save_folder, search_for+str(count)+'.jpg'))
+                else:
+                    raise TypeError
+            except Exception as e:              #catches type error along with other errors
+                print(f'fail with error {e}')
+
+            count+=1
+    
+    driver.close()
+    
+if __name__ == "__main__":
+    main()
+    
